4a2bb0403d5f8f96ce40e590dca8786a1f470c7b
Egemen <egmnakts@gmail.com>
2023-09-02 14:47:16 +0200

    initial commit

---
+++ webpack.config.cjs
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ejs$/,
+        use: 'ejs-loader',
+      },
+    ],
+  },
+};
