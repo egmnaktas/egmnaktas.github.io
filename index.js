d99188e70df2145202a9c951351f3db927a30021
Egemen <egmnakts@gmail.com>
2023-09-02 13:28:08 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-// Import necessary libraries
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import path from "path"; // Import the 'path' module
 
-// Create an Express app
 const app = express();
 const port = 3000;
-
-// API URL for fetching random cocktails
 const API_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Define a route handler for the root URL ("/")
+// Set the views directory for EJS templates
+app.set("views", path.join(__dirname, "views")); // Assuming your EJS template is in a "views" folder
+app.set("view engine", "ejs"); // Set EJS as the template engine
+
+// Route handler for the root URL ("/")
 app.get("/", async (req, res) => {
   try {
-    
-    // Fetch data from the external API
     const response = await axios.get(API_URL);
     const result = response.data;
-    
-    // Render the EJS view and pass data to it
-    res.render("index.ejs", {cocktail: result.drinks[0].strDrink, cocktailImage: result.drinks[0].strDrinkThumb, drink: result.drinks[0], cocktailIns:  result.drinks[0].strInstructions});
+
+    // Render the "index.ejs" template with data
+    res.render("index", {
+      cocktail: result.drinks[0].strDrink,
+      cocktailImage: result.drinks[0].strDrinkThumb,
+      drink: result.drinks[0],
+      cocktailIns: result.drinks[0].strInstructions,
+    });
   } catch (error) {
     console.error("Error fetching data from API:", error);
-    // Render an error page or provide an error message to users
     res.status(500).send("Internal Server Error");
   }
-})
+});
 
-
-// Start the server and listen on the specified port
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
