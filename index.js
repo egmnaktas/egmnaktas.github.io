4b3141e0eaae65d69c80faf6bab9baeebe84d627
Egemen <egmnakts@gmail.com>
2023-09-02 15:22:12 +0200

    initial commit

--- index.js
+++ index.js
@@ -22,24 +22,28 @@ app.set("view engine", "ejs"); // Set EJS as the template engine
 
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Define a route handler for the root URL ("/")
 app.get("/", async (req, res) => {
   try {
-    
     // Fetch data from the external API
     const response = await axios.get(API_URL);
     const result = response.data;
     
     // Render the EJS view and pass data to it
-    res.render("pages/index.ejs", {cocktail: result.drinks[0].strDrink, cocktailImage: result.drinks[0].strDrinkThumb, drink: result.drinks[0], cocktailIns:  result.drinks[0].strInstructions});
+    res.render("pages/index.ejs", {
+      cocktail: result.drinks[0].strDrink,
+      cocktailImage: result.drinks[0].strDrinkThumb,
+      drink: result.drinks[0],
+      cocktailIns: result.drinks[0].strInstructions
+    });
   } catch (error) {
     console.error("Error fetching data from API:", error);
     // Render an error page or provide an error message to users
     res.status(500).send("Internal Server Error");
   }
-})
+});
 
 
 // Start the server and listen on the specified port
